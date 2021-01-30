682d50673dc4a4bef09e2d7f139529713e74636a
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-01-29 22:11:07 -0600

    Se creo la carpeta components y dentro el componente Result.jsx para un uso correcto de la creación de la app calculadora

---
+++ src/components/Result.jsx
@@ -0,0 +1,15 @@
+//importación de librerias
+import React from 'react'
+
+//Arrow Function del componente o construcción del componente funcional
+const Result = () => {
+    console.log('Renderizado del componente Result.jsx')
+    return(
+        <div className="result">
+            <span> 0 </span>
+        </div>
+    )
+} 
+
+//Exportación del componente
+export default Result
