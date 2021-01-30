c24d54e60d4aabc8fa84c50a0403dcab948359e0
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-01-29 21:21:29 -0600

    Se importó el archivo App.css al componente App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 //importación de librerias y compornentes
 import React from 'react'
-
+import './App.css'
 
 //función del componente Arrow Function o funcion flecha son el standar de ECMASCRIPT 6
 // () Son la parte de la función que espera los parametros
 // {} Son la para indicar el cuerpo de la función
 //return () envía el código JSX para ser renderizado por el componente index.jsx
 const App = () => {
+    //Mensaje a la consola del navegador
+    console.log('Renderizado del primer componente App.jsx')
     return (
-        <h1> Hola Mundo desde ReactJS!!!</h1>
+        <div className="react-calculator"> 
+            Hola Mundo desde ReactJS!!!
+        </div>
     )
 }
 
