138f157647da9d270572ced87eb93a9f2a0b5002
Joaquin Antonio Sayago Trujillo <isc.jsayago@hotmail.com>
2021-01-29 21:33:12 -0600

    Se agregaron las funciones de la calculadora al componente App.jsx de la manera incorrecta como trabaja ReactJS

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,32 @@ const App = () => {
     //Mensaje a la consola del navegador
     console.log('Renderizado del primer componente App.jsx')
     return (
-        <div className="react-calculator"> 
-            Hola Mundo desde ReactJS!!!
-        </div>
+        <main className="react-calculator"> 
+            <div className="result"></div>
+            <div className="number">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>10</button>
+            </div>
+            <div className="functions">
+                <button>clear</button>
+                <button>r</button>
+            </div>
+            <div className="math-opations">
+                <button>+</button>
+                <button>-</button>
+                <button>*</button>
+                <button>/</button>
+                <button>=</button>
+            </div>
+        </main>
     )
 }
 
